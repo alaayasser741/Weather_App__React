45092ab31fde5e85a180b5a5e7fcd9278717a736
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-08 14:25:32 +0200

    get data from API

--- src/App.js
+++ src/App.js
@@ -2,16 +2,49 @@ import React, { Component } from "react";
 import "./App.css";
 import Form from "./components/form";
 import Weather from "./components/weather";
+
+const apiKey = "2696971533250289519d3f9d336861e7";
 class App extends Component {
-  state = {};
-  getWeather = (e) => {
+  state = {
+    temp: "",
+    city: "",
+    country: "",
+    hum: "",
+    desc: "",
+    error: "",
+  };
+  getWeather = async (e) => {
     e.preventDefault();
-    console.log("f");
+    let city = e.target.elements.city.value;
+    let country = e.target.elements.country.value;
+    const api = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`
+    );
+    const data = await api.json();
+    if (city & country) {
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        hum: data.main.humidity,
+        desc: data.weather[0].description,
+        error: "",
+      });
+    } else {
+      this.setState({
+        temp: "",
+        city: "",
+        country: "",
+        hum: "",
+        desc: "",
+        error: "Please Enter Right Country or City",
+      });
+    }
   };
   render() {
     return (
       <div className="App">
-        <Form getWeather={this.getWeather}/>
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 const Form = (props) => {
     return (
         <form onSubmit={props.getWeather}>
-            <input type="text" placeholder="City..." />
-            <input type="text" placeholder="Country..." />
+            <input type="text" placeholder="City..." name="city"/>
+            <input type="text" placeholder="Country..." name="country"/>
             <input type="submit" value="Check" />
         </form>
     );
