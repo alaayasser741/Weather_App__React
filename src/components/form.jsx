d320d36fce55f4bc369eb14f9193dfb1793e1601
Alaa Yasser <alaayasser2018@gmail.com>
2023-02-08 13:31:07 +0200

    init Components

---
+++ src/components/form.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                form
+            </div>
+        );
+    }
+}
+
+export default Form;
---
+++ src/components/weather.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Weather extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                weather
+            </div>
+        );
+    }
+}
+
+export default Weather;
